135ff595e62409f156f4d855a7455056ae437d68
waleed2000× <waleedahmed2000x@gmail.com>
2023-08-31 20:15:23 +0500

    Styling

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+export default function Card() {
+  return (
+    <div className="card">
+      <div className="detail">
+        <h4>
+          You: <span>Waleed</span>
+        </h4>
+        <h4>
+          Friend: <span>Someone</span>
+        </h4>
+        <p>💰:</p>
+      </div>
+      <button>X</button>
+    </div>
+  );
+}
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,17 +1,12 @@
 import Button from "./Button";
+import Card from "./Card";
 
 export default function Parent() {
   return (
     <div className="parent">
       <div className="mainContainer">
         <div className="title">List of Pending Orders</div>
-        <div className="card">
-          <div className="detail">
-            <h4>You: </h4>
-            <h4>Friend: </h4>
-          </div>
-          <button>X</button>
-        </div>
+        <Card />
         <div className="buttons">
           <Button css="add">Add</Button>
           <Button css="reset">Reset</Button>
